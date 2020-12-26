f42d4e32a4c17a473cc4977745b0dd833d21c0ec
buildingsoftwareblocks <buildingsoftwareblocks@gmail.com>
2020-12-26 16:33:51 +0100

    sonarcloud warning removed

--- shared/src/main/java/com/btb/exchange/shared/utils/CurrencyPairUtils.java
+++ shared/src/main/java/com/btb/exchange/shared/utils/CurrencyPairUtils.java
@@ -1,5 +1,6 @@
 package com.btb.exchange.shared.utils;
 
+import lombok.experimental.UtilityClass;
 import org.knowm.xchange.currency.CurrencyPair;
 
 import java.util.Arrays;
@@ -7,6 +8,7 @@ import java.util.List;
 
 import static org.knowm.xchange.currency.CurrencyPair.*;
 
+@UtilityClass
 public class CurrencyPairUtils {
     public static final List<CurrencyPair> CurrencyPairs = Arrays.asList(BTC_USDT, ETH_BTC, DASH_USDT);
 

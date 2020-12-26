e12a0ca5ad0b36b2ffb5bfd08b1046234c922326
buildingsoftwareblocks <buildingsoftwareblocks@gmail.com>
2020-12-26 16:28:38 +0100

    improve test coverage

--- shared/src/test/java/com/btb/exchange/shared/utils/TopicUtilsTest.java
+++ shared/src/test/java/com/btb/exchange/shared/utils/TopicUtilsTest.java
@@ -5,12 +5,27 @@ import org.knowm.xchange.currency.CurrencyPair;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TopicUtilsTest {
 
     @Test
+    void orderBookString() {
+        assertThat(TopicUtils.orderBook(CurrencyPair.BTC_USD.toString()), equalTo("orderbook.input.BTC_USD"));
+    }
+
+    @Test
+    void orderBookStringNull() {
+        assertThrows(NullPointerException.class, ()-> TopicUtils.orderBook((String)null));
+    }
+
+    @Test
     void orderBook() {
         assertThat(TopicUtils.orderBook(CurrencyPair.BTC_USD), equalTo("orderbook.input.BTC_USD"));
+    }
+
+    @Test
+    void orderBookNull() {
+        assertThrows(NullPointerException.class, ()-> TopicUtils.orderBook((CurrencyPair) null));
     }
 }

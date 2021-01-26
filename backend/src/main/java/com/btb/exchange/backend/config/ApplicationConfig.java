40a652e8789174a9eef132c75eaa323e36b700da
buildingsoftwareblocks <buildingsoftwareblocks@gmail.com>
2021-01-26 21:12:14 +0100

    Logging via ELK stack

--- backend/src/main/java/com/btb/exchange/backend/config/ApplicationConfig.java
+++ backend/src/main/java/com/btb/exchange/backend/config/ApplicationConfig.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-
 import javax.annotation.PostConstruct;
 
 @Configuration

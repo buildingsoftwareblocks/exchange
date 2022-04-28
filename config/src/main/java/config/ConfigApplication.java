d5e3db23ab238835ef13b646360915c1385013cb
buildingsoftwareblocks <buildingsoftwareblocks@gmail.com>
2022-04-28 21:22:48 +0200

    Log config properties as well

--- config/src/main/java/config/ConfigApplication.java
+++ config/src/main/java/config/ConfigApplication.java
@@ -1,12 +1,15 @@
 package config;
 
+import com.btb.exchange.Base;
 import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableConfigServer
+@ComponentScan(basePackageClasses = Base.class)
 @Generated // prevent JoCoCo from complaining
 public class ConfigApplication {
 

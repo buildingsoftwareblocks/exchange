210cde15ab3d89448709e67362de06cf9bbfe5a1
buildingsoftwareblocks <mark.wigmans@gmail.com>
2023-04-08 22:34:34 +0200

    process sonarcloud comment

--- shared/src/main/java/com/btb/exchange/shared/actuator/DependencyListInfoContributor.java
+++ shared/src/main/java/com/btb/exchange/shared/actuator/DependencyListInfoContributor.java
@@ -1,7 +1,5 @@
 package com.btb.exchange.shared.actuator;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import org.cyclonedx.parsers.JsonParser;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,37 +9,39 @@ import org.springframework.core.io.Resource;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 class DependencyListInfoContributor implements InfoContributor, InitializingBean {
-  private final Resource bomFile;
-  private final JsonParser jsonParser = new JsonParser();
-  private @Nullable List<Dependency> dependencies;
+    private final Resource bomFile;
+    private final JsonParser jsonParser = new JsonParser();
+    private @Nullable List<Dependency> dependencies;
 
-  DependencyListInfoContributor(@Value("classpath:bom.json") Resource bomFile) {
-    this.bomFile = bomFile;
-  }
-
-  @Override
-  public void contribute(Info.Builder builder) {
-    if (dependencies != null) {
-      builder.withDetail("dependencies", dependencies);
+    DependencyListInfoContributor(@Value("classpath:bom.json") Resource bomFile) {
+        this.bomFile = bomFile;
     }
-  }
 
-  @Override
-  public void afterPropertiesSet() throws Exception {
-    if (bomFile.exists()) {
-      try (var is = bomFile.getInputStream()) {
-        var bom = jsonParser.parse(is);
-        this.dependencies =
-            bom.getComponents().stream().map(Dependency::new).collect(Collectors.toList());
-      }
+    @Override
+    public void contribute(Info.Builder builder) {
+        if (dependencies != null) {
+            builder.withDetail("dependencies", dependencies);
+        }
     }
-  }
 
-  record Dependency(String groupId, String artifactId, String version) {
-    Dependency(org.cyclonedx.model.Component component) {
-      this(component.getGroup(), component.getName(), component.getVersion());
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if (bomFile.exists()) {
+            try (var is = bomFile.getInputStream()) {
+                var bom = jsonParser.parse(is);
+                this.dependencies =
+                        bom.getComponents().stream().map(Dependency::new).toList();
+            }
+        }
     }
-  }
+
+    record Dependency(String groupId, String artifactId, String version) {
+        Dependency(org.cyclonedx.model.Component component) {
+            this(component.getGroup(), component.getName(), component.getVersion());
+        }
+    }
 }

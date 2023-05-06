ee1da56caad9bbb6a4da7fe9a0f5cc15fec31fe1
buildingsoftwareblocks <mark.wigmans@gmail.com>
2023-05-06 12:35:57 +0200

    5 attempt build native image

--- config/src/main/java/com/btb/exchange/config/ConfigApplication.java
+++ config/src/main/java/com/btb/exchange/config/ConfigApplication.java
@@ -1,6 +1,5 @@
 package com.btb.exchange.config;
 
-import com.btb.exchange.shared.annotation.EnableCommonComponents;
 import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,11 +7,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableCommonComponents
 @Generated // prevent JoCoCo from complaining
 public class ConfigApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(ConfigApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
 }

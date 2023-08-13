df84451c528ef71697d993cfa69715b878747f78
buildingsoftwareblocks <mark.wigmans@gmail.com>
2023-08-13 15:50:20 +0200

    update java melody

--- shared/src/main/java/com/btb/exchange/shared/config/ApmConfig.java
+++ shared/src/main/java/com/btb/exchange/shared/config/ApmConfig.java
@@ -1,6 +1,7 @@
 package com.btb.exchange.shared.config;
 
 import jakarta.annotation.PreDestroy;
+import java.net.URL;
 import lombok.extern.slf4j.Slf4j;
 import net.bull.javamelody.MonitoringFilter;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,50 +9,46 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import java.net.URL;
-
 @Component
 @Slf4j
 public class ApmConfig {
 
-    @Value("${exchange.apm.enabled:false}")
-    private boolean enabled;
+  @Value("${exchange.apm.enabled:false}")
+  private boolean enabled;
 
-    @Value("${exchange.apm.server:}")
-    private String server;
+  @Value("${exchange.apm.server:}")
+  private String server;
 
-    @Value("${exchange.apm.node:http://${spring.cloud.client.ip-address:}:${server.port:}}")
-    private String node;
+  @Value("${exchange.apm.node:http://${spring.cloud.client.ip-address:}:${server.port:}}")
+  private String node;
 
-    @Value("${spring.application.name:}")
-    private String applicationName;
+  @Value("${spring.application.name:}")
+  private String applicationName;
 
-    /**
-     * Use event listener instead of PostConstruct to support lazy loaded applications as well
-     */
-    @EventListener
-    public void postConstruct(ContextRefreshedEvent ignoredEvent) {
-        if (enabled) {
-            try {
-                final URL collectServerUrl = new URL(server);
-                final URL applicationNodeUrl = new URL(node);
-                log.info("Connect ({}) to : {}", applicationNodeUrl, collectServerUrl);
-                MonitoringFilter.registerApplicationNodeInCollectServer(
-                        applicationName, collectServerUrl, applicationNodeUrl);
-            } catch (Throwable e) {
-                log.warn("Exception", e);
-            }
-        }
+  /** Use event listener instead of PostConstruct to support lazy loaded applications as well */
+  @EventListener
+  public void postConstruct(ContextRefreshedEvent ignoredEvent) {
+    if (enabled) {
+      try {
+        final URL collectServerUrl = new URL(server);
+        final URL applicationNodeUrl = new URL(node);
+        log.info("Connect ({}) to : {}", applicationNodeUrl, collectServerUrl);
+        MonitoringFilter.registerApplicationNodeInCollectServer(
+            applicationName, collectServerUrl, applicationNodeUrl);
+      } catch (Throwable e) {
+        log.warn("Exception", e);
+      }
     }
+  }
 
-    @PreDestroy
-    public void preDestruct() {
-        if (enabled) {
-            try {
-                MonitoringFilter.unregisterApplicationNodeInCollectServer();
-            } catch (Throwable e) {
-                log.warn("Exception", e);
-            }
-        }
+  @PreDestroy
+  public void preDestruct() {
+    if (enabled) {
+      try {
+        MonitoringFilter.unregisterApplicationNodeInCollectServer();
+      } catch (Throwable e) {
+        log.warn("Exception", e);
+      }
     }
+  }
 }

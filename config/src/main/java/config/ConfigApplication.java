5e272ea2cad39faa4c7376826969211739fc501a
buildingsoftwareblocks <mark.wigmans@gmail.com>
2023-09-19 21:38:15 +0200

    improved setup

--- config/src/main/java/config/ConfigApplication.java
+++ config/src/main/java/config/ConfigApplication.java
@@ -4,16 +4,15 @@ import com.btb.exchange.shared.annotation.EnableCommonComponents;
 import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.hazelcast.HazelcastAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication(exclude = {HazelcastAutoConfiguration.class})
-@EnableConfigServer
+@SpringBootApplication
 @EnableCommonComponents
+@EnableConfigServer
 @Generated // prevent JoCoCo from complaining
 public class ConfigApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(ConfigApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
 }

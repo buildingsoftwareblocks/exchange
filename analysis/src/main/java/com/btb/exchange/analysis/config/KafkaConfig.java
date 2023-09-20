ab05b94510b810065c1f1d277624e99a7ce7e0e8
buildingsoftwareblocks <mark.wigmans@gmail.com>
2023-09-20 18:05:50 +0200

    improved setup

--- analysis/src/main/java/com/btb/exchange/analysis/config/KafkaConfig.java
+++ analysis/src/main/java/com/btb/exchange/analysis/config/KafkaConfig.java
@@ -13,28 +13,29 @@ import org.springframework.kafka.core.ConsumerFactory;
 @RequiredArgsConstructor
 public class KafkaConfig {
 
-  private final ConsumerFactory<String, String> consumerFactory;
+    private final ConsumerFactory<String, String> consumerFactory;
 
-  @Value("${analysis.kafka.partitions:10}")
-  private int partitions;
+    @Value("${analysis.kafka.partitions:10}")
+    private int partitions;
 
-  @Value("${analysis.kafka.concurrency:5}")
-  private int concurrency;
+    @Value("${analysis.kafka.concurrency:1}")
+    private int concurrency;
 
-  @Value("${analysis.kafka.replication:1}")
-  private short replication;
+    @Value("${analysis.kafka.replication:1}")
+    private short replication;
 
-  @Bean
-  public NewTopic opportunities() {
-    return new NewTopic(TopicUtils.OPPORTUNITIES, partitions, replication);
-  }
+    @Bean
+    public NewTopic opportunities() {
+        return new NewTopic(TopicUtils.OPPORTUNITIES, partitions, replication);
+    }
 
-  @Bean
-  public ConcurrentKafkaListenerContainerFactory<String, String> batchFactory() {
-    ConcurrentKafkaListenerContainerFactory<String, String> factory =
-        new ConcurrentKafkaListenerContainerFactory<>();
-    factory.setConsumerFactory(consumerFactory);
-    factory.setConcurrency(concurrency);
-    return factory;
-  }
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> batchFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setConcurrency(concurrency);
+        factory.setBatchListener(true);
+        return factory;
+    }
 }

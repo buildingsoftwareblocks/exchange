1fea176dd96862d34e630b257f2153a6a49d5c53
buildingsoftwareblocks <mark.wigmans@gmail.com>
2023-09-29 09:07:52 +0200

    APM to early requested

--- shared/src/main/java/com/btb/exchange/shared/config/ApmConfig.java
+++ shared/src/main/java/com/btb/exchange/shared/config/ApmConfig.java
@@ -1,13 +1,14 @@
 package com.btb.exchange.shared.config;
 
-import java.net.URL;
-import javax.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import net.bull.javamelody.MonitoringFilter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
+import java.net.URL;
 
 @Component
 @Slf4j
@@ -25,9 +26,11 @@ public class ApmConfig {
     @Value("${spring.application.name:}")
     private String applicationName;
 
-    /** Use event listener instead of PostConstruct to support lazy loaded applications as well */
+    /**
+     * Use event listener instead of PostConstruct to support lazy loaded applications as well
+     */
     @EventListener
-    public void postConstruct(ContextRefreshedEvent ignoredEvent) {
+    public void postConstruct(ApplicationReadyEvent ignoredEvent) {
         if (enabled) {
             try {
                 final URL collectServerUrl = new URL(server);

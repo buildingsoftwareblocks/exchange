d8e7390b1447177d3ec2631700ebbef91319d995
markwigmans <mark.wigmans@gmail.com>
2025-03-21 20:55:24 +0100

    improved

--- shared/src/main/java/com/btb/exchange/shared/utils/PropertyLogger.java
+++ shared/src/main/java/com/btb/exchange/shared/utils/PropertyLogger.java
@@ -1,57 +1,68 @@
 package com.btb.exchange.shared.utils;
 
-import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.StreamSupport;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.event.ApplicationPreparedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.*;
 
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.StreamSupport;
+
 /**
  * Log all application properties
  */
 @Slf4j
-class PropertyLogger implements ApplicationListener<ApplicationPreparedEvent> {
+public class PropertyLogger implements ApplicationListener<ApplicationPreparedEvent> {
 
     private static final AtomicBoolean RUN_ONCE = new AtomicBoolean(false);
 
     @Override
     public void onApplicationEvent(ApplicationPreparedEvent event) {
         if (!RUN_ONCE.getAndSet(true)) {
-            show((event.getApplicationContext().getEnvironment()));
+            logEnvironmentDetails(event.getApplicationContext().getEnvironment());
         }
     }
 
-    private void show(Environment env) {
+    private void logEnvironmentDetails(final Environment env) {
         log.info("====== Environment and configuration ======");
-        log.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
-        final MutablePropertySources sources = ((AbstractEnvironment) env).getPropertySources();
-        log.info(
-                "Property sources: {}",
-                Arrays.toString(sources.stream()
-                        .filter(EnumerablePropertySource.class::isInstance)
-                        .map(PropertySource::getName)
-                        .toArray()));
+        logActiveProfiles(env);
+        logPropertySources(env);
+        logProperties(env);
+        log.info("===========================================");
+    }
 
-        StreamSupport.stream(sources.stream().spliterator(), false)
+    private void logActiveProfiles(final Environment env) {
+        log.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
+    }
+
+    private void logPropertySources(final Environment env) {
+        MutablePropertySources sources = ((AbstractEnvironment) env).getPropertySources();
+        String[] sourceNames = sources.stream()
                 .filter(EnumerablePropertySource.class::isInstance)
-                .map(ps -> ((EnumerablePropertySource) ps).getPropertyNames())
+                .map(PropertySource::getName)
+                .toArray(String[]::new);
+        log.info("Property sources: {}", Arrays.toString(sourceNames));
+    }
+
+    private void logProperties(final Environment env) {
+        MutablePropertySources sources = ((AbstractEnvironment) env).getPropertySources();
+        StreamSupport.stream(sources.spliterator(), false)
+                .filter(EnumerablePropertySource.class::isInstance)
+                .map(ps -> ((EnumerablePropertySource<?>) ps).getPropertyNames())
                 .flatMap(Arrays::stream)
                 .distinct()
                 .sorted()
-                .forEach(prop -> {
-                    try {
-                        if (!(StringUtils.containsIgnoreCase(prop, "password"))) {
-                            log.info("{}={}", prop, env.getProperty(prop));
-                        } else {
-                            log.info("{} = *****", prop);
-                        }
-                    } catch (Exception e) {
-                        log.warn("{} -> {}", prop, e.getMessage());
-                    }
-                });
-        log.info("===========================================");
+                .forEach(prop -> logProperty(env, prop));
+    }
+
+    private void logProperty(final Environment env, final String prop) {
+        try {
+            final String value = StringUtils.containsIgnoreCase(prop, "password") ? "*****" : env.getProperty(prop);
+            log.info("{}={}", prop, value);
+        } catch (Exception e) {
+            log.warn("Error logging property {}: {}", prop, e.getMessage());
+        }
     }
 }
